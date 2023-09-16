2211a6f41fa0d798d32c581faaf0a32b27246688
vsilent <jabberroid@gmail.com>
2023-09-16 09:59:53 +0300

    Json extractor instead Form

--- src/routes/stack/add.rs
+++ src/routes/stack/add.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web::{Data, Bytes, Json}, HttpResponse, HttpRequest, FromRequest};
 use sqlx::PgPool;
 use tracing::Instrument;
 use uuid::Uuid;
@@ -7,61 +7,68 @@ use crate::models::stack::FormData;
 use crate::startup::AppState;
 
 
-pub async fn add(app_state: web::Data<AppState>, form: web::Form<FormData>, pool:
-web::Data<PgPool>) -> HttpResponse {
-    tracing::debug!("we are here");
+pub async fn add(req: HttpRequest, app_state: Data<AppState>, pool:
+Data<PgPool>, body: Bytes) -> HttpResponse {
+    let content_type = req.headers().get("content-type");
+    println!("=================== Request Content-Type: {:?}", content_type);
+    println!("request: {:?}", body);
+    // println!("app: {:?}", body);
     tracing::info!("we are here");
+    match Json::<FormData>::extract(&req).await {
+        Ok(form) => println!("Hello from {:?}!", form),
+        Err(err) => println!("error={:?}", err),
+    };
 
-    let user_id = app_state.user_id;
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Validating a new stack", %request_id,
-        commonDomain=?form.common_domain,
-        region=?form.region,
-        domainList=?form.domain_list
-    );
-
-    // using `enter` is an async function
-    let _request_span_guard = request_span.enter(); // ->exit
-
-    tracing::info!(
-        "request_id {} Adding '{}' '{}' as a new stack",
-        request_id,
-        form.common_domain,
-        form.region
-    );
-
-    let query_span = tracing::info_span!(
-        "Saving new stack details into the database"
-    );
-
-    // match sqlx::query!(
-    //     r#"
-    //     INSERT INTO user_stack (id, user_id, name, created_at, updated_at)
-    //     VALUES ($1, $2, $3, $4, $5)
-    //     "#,
-    //     0_i32,
-    //     user_id,
+    // let user_id = app_state.user_id;
+    // let request_id = Uuid::new_v4();
+    // let request_span = tracing::info_span!(
+    //     "Validating a new stack", %request_id,
+    //     commonDomain=?form.common_domain,
+    //     region=?form.region,
+    //     domainList=?form.domain_list
+    // );
+    //
+    // // using `enter` is an async function
+    // let _request_span_guard = request_span.enter(); // ->exit
+    //
+    // tracing::info!(
+    //     "request_id {} Adding '{}' '{}' as a new stack",
+    //     request_id,
     //     form.common_domain,
-    //     Utc::now(),
-    //     Utc::now()
-    // )
-    // .execute(pool.get_ref())
-    // .instrument(query_span)
-    // .await
-    // {
-    //     Ok(_) => {
-    //         tracing::info!(
-    //             "req_id: {} New stack details have been saved to database",
-    //             request_id
-    //         );
-    //         HttpResponse::Ok().finish()
-    //     }
-    //     Err(e) => {
-    //         tracing::error!("req_id: {} Failed to execute query: {:?}", request_id, e);
-    //         HttpResponse::InternalServerError().finish()
-    //     }
-    // }
+    //     form.region
+    // );
+    //
+    // let query_span = tracing::info_span!(
+    //     "Saving new stack details into the database"
+    // );
+    //
+    // // match sqlx::query!(
+    // //     r#"
+    // //     INSERT INTO user_stack (id, user_id, name, created_at, updated_at)
+    // //     VALUES ($1, $2, $3, $4, $5)
+    // //     "#,
+    // //     0_i32,
+    // //     user_id,
+    // //     form.common_domain,
+    // //     Utc::now(),
+    // //     Utc::now()
+    // // )
+    // // .execute(pool.get_ref())
+    // // .instrument(query_span)
+    // // .await
+    // // {
+    // //     Ok(_) => {
+    // //         tracing::info!(
+    // //             "req_id: {} New stack details have been saved to database",
+    // //             request_id
+    // //         );
+    // //         HttpResponse::Ok().finish()
+    // //     }
+    // //     Err(e) => {
+    // //         tracing::error!("req_id: {} Failed to execute query: {:?}", request_id, e);
+    // //         HttpResponse::InternalServerError().finish()
+    // //     }
+    // // }
 
     HttpResponse::Ok().finish()
 }
--- src/startup.rs
+++ src/startup.rs
@@ -7,8 +7,10 @@ use actix_web::{
 };
 use sqlx::PgPool;
 use std::net::TcpListener;
+use serde_derive::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct AppState {
    pub user_id: i32 // @todo User must be move later to actix session and obtained from auth
 }
