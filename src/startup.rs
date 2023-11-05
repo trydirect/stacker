8e06d2ace1c1ae1912826a8afd3bc450707c6995
Petru <smart__petea@mail.ru>
2023-11-05 13:55:27 +0200

    issue-auth move middleware

--- src/startup.rs
+++ src/startup.rs
@@ -1,77 +1,15 @@
 use crate::configuration::Settings;
-use crate::forms::user::UserForm;
 use actix_cors::Cors;
-use actix_web::dev::{Server, ServiceRequest};
-use actix_web::error::{ErrorInternalServerError, ErrorUnauthorized};
-use actix_web::HttpMessage;
+use actix_web::dev::Server;
 use actix_web::{
     web::{self},
-    App, Error, HttpServer,
+    App, HttpServer,
 };
-use actix_web_httpauth::{extractors::bearer::BearerAuth, middleware::HttpAuthentication};
-use reqwest::header::{ACCEPT, CONTENT_TYPE};
+use actix_web_httpauth::middleware::HttpAuthentication;
 use sqlx::{Pool, Postgres};
 use std::net::TcpListener;
 use std::sync::Arc;
 use tracing_actix_web::TracingLogger;
-
-use crate::models::user::User;
-
-#[tracing::instrument(name = "Bearer guard.")]
-async fn bearer_guard(
-    req: ServiceRequest,
-    credentials: BearerAuth,
-) -> Result<ServiceRequest, (Error, ServiceRequest)> {
-    let settings = req.app_data::<web::Data<Arc<Settings>>>().unwrap();
-    let client = reqwest::Client::new();
-    let resp = client
-        .get(&settings.auth_url)
-        .bearer_auth(credentials.token())
-        .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
-        .send()
-        .await;
-
-    let resp = match resp {
-        Ok(resp) if resp.status().is_success() => resp,
-        Ok(resp) => {
-            tracing::error!("Authentication service returned no success {:?}", resp);
-            // tracing::debug!("{:?}", resp.text().await.unwrap());
-            return Err((ErrorUnauthorized("401 Unauthorized"), req));
-        }
-        Err(err) => {
-            tracing::error!("error from reqwest {:?}", err);
-            return Err((ErrorInternalServerError(err.to_string()), req));
-        }
-    };
-
-    let user_form: UserForm = match resp.json().await {
-        Ok(user) => {
-            tracing::info!("unpacked user {user:?}");
-            user
-        }
-        Err(err) => {
-            tracing::error!("can't parse the response body {:?}", err);
-            return Err((ErrorUnauthorized(""), req));
-        }
-    };
-
-    let user: User = match user_form.try_into() // try to convert UserForm into User model
-    {
-        Ok(user)  => { user }
-        Err(err) => {
-            tracing::error!("Could not create User from form data: {:?}", err);
-            return Err((ErrorUnauthorized("Unauthorized"), req));
-        }
-    };
-    let existent_user = req.extensions_mut().insert(user);
-    if existent_user.is_some() {
-        tracing::error!("already logged {existent_user:?}");
-        return Err((ErrorInternalServerError(""), req));
-    }
-
-    Ok(req)
-}
 
 pub async fn run(
     listener: TcpListener,
@@ -92,7 +30,9 @@ pub async fn run(
             .service(web::scope("/health_check").service(crate::routes::health_check))
             .service(
                 web::scope("/client")
-                    .wrap(HttpAuthentication::bearer(bearer_guard))
+                    .wrap(HttpAuthentication::bearer(
+                        crate::middleware::trydirect::bearer_guard,
+                    ))
                     .wrap(Cors::permissive())
                     .service(crate::routes::client::add_handler)
                     .service(crate::routes::client::update_handler),
@@ -107,7 +47,9 @@ pub async fn run(
             )
             .service(
                 web::scope("/rating")
-                    .wrap(HttpAuthentication::bearer(bearer_guard))
+                    .wrap(HttpAuthentication::bearer(
+                        crate::middleware::trydirect::bearer_guard,
+                    ))
                     .wrap(Cors::permissive())
                     .service(crate::routes::rating::add_handler)
                     .service(crate::routes::rating::get_handler)
@@ -124,7 +66,9 @@ pub async fn run(
             // )
             .service(
                 web::scope("/stack")
-                    .wrap(HttpAuthentication::bearer(bearer_guard))
+                    .wrap(HttpAuthentication::bearer(
+                        crate::middleware::trydirect::bearer_guard,
+                    ))
                     .wrap(Cors::permissive())
                     .service(crate::routes::stack::add::add)
                     .service(crate::routes::stack::get::get),
