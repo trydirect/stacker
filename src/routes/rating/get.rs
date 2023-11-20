a42d40a9c05b703c68ba742244d5e4b5b3532276
Petru <smart__petea@mail.ru>
2023-11-20 18:08:37 +0200

    issue-auth rating/get.rs

--- src/routes/rating/get.rs
+++ src/routes/rating/get.rs
@@ -1,15 +1,13 @@
+use crate::helpers::JsonResponse;
 use crate::models;
 use actix_web::{get, web, Responder, Result};
-use serde_derive::Serialize;
 use sqlx::PgPool;
 use tracing::Instrument;
-use crate::helpers::JsonResponse;
 
 // workflow
 // add, update, list, get(user_id), ACL,
 // ACL - access to func for a user
 // ACL - access to objects for a user
-
 
 #[tracing::instrument(name = "Get rating.")]
 #[get("/{id}")]
@@ -30,14 +28,14 @@ pub async fn get_handler(
     {
         Ok(rating) => {
             tracing::info!("rating found: {:?}", rating.id);
-            return JsonResponse::build().set_item(Some(rating)).ok("OK".to_string());
+            return JsonResponse::build().set_item(Some(rating)).ok("OK");
         }
         Err(sqlx::Error::RowNotFound) => {
-            return JsonResponse::build().err("Not Found".to_string());
+            return JsonResponse::build().err("Not Found");
         }
         Err(e) => {
             tracing::error!("Failed to fetch rating, error: {:?}", e);
-            return JsonResponse::build().err("Internal Server Error".to_string());
+            return JsonResponse::build().err("Internal Server Error");
         }
     }
 }
@@ -57,11 +55,11 @@ pub async fn default(path: web::Path<()>, pool: web::Data<PgPool>) -> Result<imp
             return JsonResponse::build().set_list(rating).ok("OK".to_string());
         }
         Err(sqlx::Error::RowNotFound) => {
-            return JsonResponse::build().err("Not Found".to_string());
+            return JsonResponse::build().err("Not Found");
         }
         Err(e) => {
             tracing::error!("Failed to fetch rating, error: {:?}", e);
-            return JsonResponse::build().err("Internal Server Error".to_string());
+            return JsonResponse::build().err("Internal Server Error");
         }
     }
 }
