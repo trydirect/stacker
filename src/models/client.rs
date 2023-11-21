2a51b7bda23f333b6ed702d5f69101fb69099a0a
Petru <smart__petea@mail.ru>
2023-11-21 17:59:03 +0200

    issue-auth debug for models::client

--- src/models/client.rs
+++ src/models/client.rs
@@ -1,8 +1,18 @@
 use serde::Serialize;
 
-#[derive(Default, Serialize, Debug)]
+#[derive(Default, Serialize)]
 pub struct Client {
     pub id: i32,
     pub user_id: String,
     pub secret: Option<String>,
+}
+
+impl std::fmt::Debug for Client {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Client {{id: {:?}, user_id: {:?}}}",
+            self.id, self.user_id
+        )
+    }
 }
