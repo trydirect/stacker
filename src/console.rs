9643121f17bbe0a745e01f2637326413fd498dac
Petru <smart__petea@mail.ru>
2023-11-26 09:11:11 +0200

    command

--- src/console.rs
+++ src/console.rs
@@ -1,3 +1,40 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    AppClient {
+        #[command(subcommand)]
+        command: AppClientCommands,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum AppClientCommands {
+    New {
+        #[arg(long)]
+        user_id: i32,
+    },
+}
+
 fn main() {
-    println!("Hello World!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::AppClient { command } => {
+            process_app_client_command(command);
+        }
+        _ => {
+            println!("other variant");
+        }
+    };
+}
+
+fn process_app_client_command(command: AppClientCommands) {
+    println!("{command:?}");
 }
