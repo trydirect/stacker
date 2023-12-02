14a8a367c89578d4a111e5af17938cb6e081424e
Petru <smart__petea@mail.ru>
2023-12-02 18:15:58 +0200

    issue-auth test middleware_client sketch

---
+++ tests/middleware_client.rs
@@ -0,0 +1,22 @@
+mod common;
+
+#[tokio::test]
+async fn middleware_client_works() {
+    // 1. Arrange
+    // 2. Act
+    // 3. Assert
+
+    println!("Before spawn_app");
+    let app = common::spawn_app().await; // server
+    println!("After spawn_app");
+    let client = reqwest::Client::new(); // client
+
+    let response = client
+        .get(&format!("{}/health_check", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
