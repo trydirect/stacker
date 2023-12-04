5ba752b9eac47027c3d8564212576603371a7d79
petru <smart__petea@mail.ru>
2023-12-04 17:42:13 +0200

    issue-auth optimising middleware client

--- src/middleware/trydirect.rs
+++ src/middleware/trydirect.rs
@@ -1,4 +1,5 @@
 use crate::configuration::Settings;
+use futures::future::{FutureExt};
 use crate::forms::user::UserForm;
 use actix_web::dev::ServiceRequest;
 use actix_web::error::{ErrorInternalServerError, ErrorUnauthorized};
@@ -7,59 +8,69 @@ use actix_web::Error;
 use actix_web::HttpMessage;
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use reqwest::header::{ACCEPT, CONTENT_TYPE};
+use crate::helpers::JsonResponse;
 
-use crate::models::user::User;
+use crate::models::user::User; //todo
 
 #[tracing::instrument(name = "Trydirect bearer guard.")]
 pub async fn bearer_guard(
     req: ServiceRequest,
     credentials: BearerAuth,
-) -> Result<ServiceRequest, (Error, ServiceRequest)> {
-    let settings = req.app_data::<web::Data<Settings>>().unwrap();
-    let client = reqwest::Client::new();
-    let resp = client
-        .get(&settings.auth_url)
-        .bearer_auth(credentials.token())
-        .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
-        .send()
-        .await;
+    ) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    async {
+        let settings = req.app_data::<web::Data<Settings>>().unwrap();
+        let client = reqwest::Client::new();
+        let resp = client
+            .get(&settings.auth_url)
+            .bearer_auth(credentials.token())
+            .header(CONTENT_TYPE, "application/json")
+            .header(ACCEPT, "application/json")
+            .send()
+            .await;
 
-    let resp = match resp {
-        Ok(resp) if resp.status().is_success() => resp,
-        Ok(resp) => {
-            tracing::error!("Authentication service returned no success {:?}", resp);
-            return Err((ErrorUnauthorized("401 Unauthorized"), req));
-        }
-        Err(err) => {
-            tracing::error!("error from reqwest {:?}", err);
-            return Err((ErrorInternalServerError(err.to_string()), req));
-        }
-    };
+        let resp = match resp {
+            Ok(resp) if resp.status().is_success() => resp,
+            Ok(resp) => {
+                tracing::error!("Authentication service returned no success {:?}", resp);
+                return Err(("401 Unauthorized".to_string(), req));
+            }
+            Err(err) => {
+                tracing::error!("error from reqwest {:?}", err);
+                return Err((err.to_string(), req));
+            }
+        };
 
-    let user_form: UserForm = match resp.json().await {
-        Ok(user) => {
-            tracing::info!("unpacked user {user:?}");
-            user
-        }
-        Err(err) => {
-            tracing::error!("can't parse the response body {:?}", err);
-            return Err((ErrorUnauthorized(""), req));
-        }
-    };
+        let user_form: UserForm = match resp.json().await {
+            Ok(user) => {
+                tracing::info!("unpacked user {user:?}");
+                user
+            }
+            Err(err) => {
+                tracing::error!("can't parse the response body {:?}", err);
+                return Err(("can't parse the response body".to_string(), req));
+            }
+        };
 
-    let user: User = match user_form.try_into() {
-        Ok(user) => user,
-        Err(err) => {
-            tracing::error!("Could not create User from form data: {:?}", err);
-            return Err((ErrorUnauthorized("Unauthorized"), req));
+        let user: User = match user_form.try_into() {
+            Ok(user) => user,
+            Err(err) => {
+                tracing::error!("Could not create User from form data: {:?}", err);
+                return Err(("Could not create User from form data".to_string(), req));
+            }
+        };
+        let existent_user = req.extensions_mut().insert(user);
+        if existent_user.is_some() {
+            tracing::error!("already logged {existent_user:?}");
+            return Err(("user already logged".to_string(), req));
         }
-    };
-    let existent_user = req.extensions_mut().insert(user);
-    if existent_user.is_some() {
-        tracing::error!("already logged {existent_user:?}");
-        return Err((ErrorInternalServerError(""), req));
-    }
 
-    Ok(req)
+        Ok(req)
+    }.await
+    .map_err(|(err, req)| {
+        tracing::error!("Authentication service returned no success {:?}", err);
+        (ErrorUnauthorized(JsonResponse::<i32>::build().set_msg(err).to_string()), req) //todo
+                                                                                        //default
+                                                                                        //type for
+                                                                                        //JsonResponse
+    })
 }
