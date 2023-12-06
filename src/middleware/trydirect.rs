4ca2589ffce150454be9110059be7104a4230a3b
petru <smart__petea@mail.ru>
2023-12-06 08:32:44 +0200

    issue-auth trydirect middleware

--- src/middleware/trydirect.rs
+++ src/middleware/trydirect.rs
@@ -15,40 +15,38 @@ use crate::models::user::User; //todo
 #[tracing::instrument(name = "Trydirect bearer guard.")]
 pub async fn bearer_guard( req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, (Error, ServiceRequest)> {
     let settings = req.app_data::<web::Data<Settings>>().unwrap();
-    async move {
-        let client = reqwest::Client::new();
-        let resp = client
-            .get(&settings.auth_url)
-            .bearer_auth(credentials.token())
-            .header(CONTENT_TYPE, "application/json")
-            .header(ACCEPT, "application/json")
-            .send()
-            .await
-            .map_err(|err| "no resp from auth server".to_string())?;
+    let token = credentials.token();
+    let user = fetch_user(settings.auth_url.as_str(), token).await;
+    if let Err(err) = user {
+        return Err((ErrorUnauthorized(JsonResponse::<i32>::build().set_msg(err).to_string()), req));
+    }
 
-        if !resp.status().is_success() {
-            return Err("401 Unauthorized".to_string());
-        }
+    let user = user.unwrap();
+    if req.extensions_mut().insert(user).is_some() {
+        return Err((ErrorUnauthorized(JsonResponse::<i32>::build().set_msg("user already logged").to_string()), req));
+    }
 
-        resp
-            .json::<UserForm>()
-            .await
-            .map_err(|err| "can't parse the response body".to_string())?
-            .try_into() //User"Could not create User from form data"
-    }.await
-    .map(move |result| {
-        match result {
-            Ok(user) => {
-                let existent_user = req.extensions_mut().insert(user);
-                if existent_user.is_some() {
-                    return Err((ErrorUnauthorized(JsonResponse::<i32>::build().set_msg("user already logged").to_string()), req));
-                }
+    Ok(req)
+}
 
-                return Ok(req);
-            }
-            Err(err) => {
-                return Err((ErrorUnauthorized(JsonResponse::<i32>::build().set_msg(err).to_string()), req));
-            }
-        }
-    })
+async fn fetch_user(auth_url: &str, token: &str) -> Result<User, String> {
+    let client = reqwest::Client::new();
+    let resp = client
+        .get(auth_url)
+        .bearer_auth(token)
+        .header(CONTENT_TYPE, "application/json")
+        .header(ACCEPT, "application/json")
+        .send()
+        .await
+        .map_err(|err| "no resp from auth server".to_string())?;
+
+    if !resp.status().is_success() {
+        return Err("401 Unauthorized".to_string());
+    }
+
+    resp
+        .json::<UserForm>()
+        .await
+        .map_err(|err| "can't parse the response body".to_string())?
+        .try_into()
 }
