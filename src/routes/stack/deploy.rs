874162b8272890cbc378379cfd06fcefd6ff045a
Petru <smart__petea@mail.ru>
2023-12-26 18:21:47 +0200

    issue-auth stack/deploy

--- src/routes/stack/deploy.rs
+++ src/routes/stack/deploy.rs
@@ -1,9 +1,9 @@
 use crate::configuration::Settings;
+use crate::db;
 use crate::forms::StackPayload;
 use crate::helpers::stack::builder::DcBuilder;
 use crate::helpers::JsonResponse;
-use crate::models::stack::Stack;
-use crate::models::user::User;
+use crate::models;
 use actix_web::{post, web, web::Data, Responder, Result};
 use futures_lite::stream::StreamExt;
 use lapin::{
@@ -12,89 +12,66 @@ use lapin::{
 use sqlx::PgPool;
 use std::sync::Arc;
 
-
 #[tracing::instrument(name = "Deploy for every user. Admin endpoint")]
 #[post("/{id}/deploy")]
 pub async fn add(
-    user: web::ReqData<Arc<User>>,
+    user: web::ReqData<Arc<models::User>>,
     path: web::Path<(i32,)>,
     pool: Data<PgPool>,
     sets: Data<Settings>,
 ) -> Result<impl Responder> {
     let id = path.0;
-    tracing::debug!("Received id: {}", id);
+    let stack = db::stack::fetch(pool.get_ref(), id)
+        .await
+        .map_err(|err| JsonResponse::<models::Stack>::build().internal_server_error(err))
+        .and_then(|stack| match stack {
+            Some(stack) => Ok(stack),
+            None => Err(JsonResponse::<models::Stack>::build().not_found("not found")),
+        })?;
 
-    let stack = match sqlx::query_as!(
-        Stack,
-        r#"
-        SELECT * FROM user_stack WHERE id=$1 LIMIT 1
-        "#,
-        id
-    )
-    .fetch_one(pool.get_ref())
-    .await
-    {
-        Ok(stack) => {
-            tracing::info!("Stack found: {:?}", stack.id,);
-            Some(stack)
-        }
-        Err(sqlx::Error::RowNotFound) => {
-            tracing::error!("Row not found 404");
-            None
-        }
-        Err(e) => {
-            tracing::error!("Failed to fetch stack, error: {:?}", e);
-            None
-        }
-    };
+    let id = stack.id.clone();
+    let dc = DcBuilder::new(stack);
+    dc.build().ok_or_else(|| {
+        tracing::error!("Error. Compose builder returned an empty string");
+        JsonResponse::<models::Stack>::build().internal_server_error("troubles at building")
+    })?;
 
-    return match stack {
-        Some(stack) => {
-            let id = stack.id.clone();
-            let dc = DcBuilder::new(stack);
-            dc.build();
+    let addr = sets.amqp.connection_string();
+    let routing_key = "install.start.tfa.all.all".to_string();
+    tracing::debug!("Sending message to {:?}", routing_key);
 
-            let addr = sets.amqp.connection_string();
-            let routing_key = "install.start.tfa.all.all".to_string();
-            tracing::debug!("Sending message to {:?}", routing_key);
+    let conn = Connection::connect(&addr, ConnectionProperties::default())
+        .await
+        .expect("Could not connect RabbitMQ");
 
-            let conn = Connection::connect(&addr, ConnectionProperties::default())
-                .await
-                .expect("Could not connect RabbitMQ");
+    tracing::info!("RABBITMQ CONNECTED");
 
-            tracing::info!("RABBITMQ CONNECTED");
+    let channel = conn.create_channel().await.unwrap();
+    let mut stack_data = serde_json::from_value::<StackPayload>(dc.stack.body.clone()).unwrap();
 
-            let channel = conn.create_channel().await.unwrap();
-            let mut stack_data =
-                serde_json::from_value::<StackPayload>(dc.stack.body.clone()).unwrap();
+    stack_data.id = Some(id);
+    stack_data.user_token = Some(user.id.clone());
+    stack_data.user_email = Some(user.email.clone());
+    stack_data.stack_code = stack_data.custom.custom_stack_code.clone();
 
-            stack_data.id = Some(id);
-            stack_data.user_token = Some(user.id.clone());
-            stack_data.user_email = Some(user.email.clone());
-            stack_data.stack_code = stack_data.custom.custom_stack_code.clone();
+    let payload = serde_json::to_string::<StackPayload>(&stack_data).unwrap();
+    let _payload = payload.as_bytes();
 
-            let payload = serde_json::to_string::<StackPayload>(&stack_data).unwrap();
-            let _payload = payload.as_bytes();
+    let confirm = channel
+        .basic_publish(
+            "install",
+            routing_key.as_str(),
+            BasicPublishOptions::default(),
+            _payload,
+            BasicProperties::default(),
+        )
+        .await
+        .unwrap()
+        .await
+        .unwrap();
 
-            let confirm = channel
-                .basic_publish(
-                    "install",
-                    routing_key.as_str(),
-                    BasicPublishOptions::default(),
-                    _payload,
-                    BasicProperties::default(),
-                )
-                .await
-                .unwrap()
-                .await
-                .unwrap();
-
-            assert_eq!(confirm, Confirmation::NotRequested);
-            tracing::debug!("Message sent to rabbitmq");
-            return Ok(JsonResponse::<Stack>::build().set_id(id).ok("Success"));
-        }
-        None => {
-            Err(JsonResponse::<Stack>::build().internal_server_error("Deployment failed"))
-        }
-    }
+    assert_eq!(confirm, Confirmation::NotRequested);
+    return Ok(JsonResponse::<models::Stack>::build()
+        .set_id(id)
+        .ok("Success"));
 }
