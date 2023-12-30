055988c936b9c76d127751461f0f4fdb2a2c0dbf
Petru <smart__petea@mail.ru>
2023-12-30 09:33:15 +0200

    issue-auth pg_pool

--- src/routes/client/update.rs
+++ src/routes/client/update.rs
@@ -1,39 +1,43 @@
+use crate::db;
 use crate::helpers::client;
 use crate::models;
-use crate::db;
 use crate::{configuration::Settings, helpers::JsonResponse};
 use actix_web::{put, web, Responder, Result};
+use futures::TryFutureExt;
 use sqlx::PgPool;
 use std::sync::Arc;
 use tracing::Instrument;
-use futures::TryFutureExt;
 
 #[tracing::instrument(name = "Update client.")]
 #[put("/{id}")]
 pub async fn update_handler(
     user: web::ReqData<Arc<models::User>>,
     settings: web::Data<Settings>,
-    pool: web::Data<PgPool>,
+    pg_pool: web::Data<PgPool>,
     path: web::Path<(i32,)>,
 ) -> Result<impl Responder> {
     let client_id = path.0;
-    let mut client = db::client::fetch(pool.get_ref(), client_id)
+    let mut client = db::client::fetch(pg_pool.get_ref(), client_id)
         .await
         .map_err(|msg| JsonResponse::<models::Client>::build().internal_server_error(msg))?
-        .ok_or_else(|| JsonResponse::<models::Client>::build().not_found("not found"))?; 
+        .ok_or_else(|| JsonResponse::<models::Client>::build().not_found("not found"))?;
 
     if client.secret.is_none() {
         return Err(JsonResponse::<models::Client>::build().bad_request("client is not active"));
     }
 
-    client.secret = client::generate_secret(pool.get_ref(), 255)
+    client.secret = client::generate_secret(pg_pool.get_ref(), 255)
         .await
         .map(|s| Some(s))
         .map_err(|msg| JsonResponse::<models::Client>::build().bad_request(msg))?;
 
-    db::client::update(pool.get_ref(), client)
+    db::client::update(pg_pool.get_ref(), client)
         .await
-        .map(|client| JsonResponse::<models::Client>::build().set_item(client).ok("success"))
+        .map(|client| {
+            JsonResponse::<models::Client>::build()
+                .set_item(client)
+                .ok("success")
+        })
         .map_err(|err| {
             tracing::error!("Failed to execute query: {:?}", err);
             JsonResponse::<models::Client>::build().internal_server_error("")
