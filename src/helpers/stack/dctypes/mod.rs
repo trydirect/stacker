c95fe1ffee0a3f17b3e6e3e478bb23a671b005e0
petru <smart__petea@mail.ru>
2024-01-14 09:50:24 +0200

    issue-auth dctypes

--- src/helpers/stack/dctypes/mod.rs
+++ src/helpers/stack/dctypes/mod.rs
@@ -27,6 +27,13 @@ mod advanced_network_settings;
 mod top_level_volumes;
 mod compose_volume;
 mod external_volume;
+mod compose_network;
+mod compose_networks;
+mod compose_network_setting_details;
+mod external_network_setting_bool;
+mod network_settings;
+mod ipam;
+mod ipam_config;
 
 pub use port::*;
 pub use published_port::*;
@@ -57,6 +64,13 @@ pub use advanced_network_settings::*;
 pub use top_level_volumes::*;
 pub use compose_volume::*;
 pub use external_volume::*;
+pub use compose_networks::*;
+pub use compose_network::*;
+pub use compose_network_setting_details::*;
+pub use external_network_setting_bool::*;
+pub use network_settings::*;
+pub use ipam::*;
+pub use ipam_config::*;
 
 use crate::helpers::stack::dctypes;
 
@@ -70,80 +84,6 @@ use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::fmt;
 
-#[cfg(feature = "indexmap")]
-#[derive(Clone, Default, Debug, Serialize, Deserialize, PartialEq)]
-pub struct ComposeNetworks(pub IndexMap<String, MapOrEmpty<NetworkSettings>>);
-
-#[cfg(not(feature = "indexmap"))]
-#[derive(Clone, Default, Debug, Serialize, Deserialize, PartialEq)]
-pub struct ComposeNetworks(pub HashMap<String, MapOrEmpty<NetworkSettings>>);
-
-impl ComposeNetworks {
-    pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, Hash)]
-#[serde(untagged)]
-pub enum ComposeNetwork {
-    Detailed(ComposeNetworkSettingDetails),
-    Bool(bool),
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, Hash)]
-#[serde(deny_unknown_fields)]
-pub struct ComposeNetworkSettingDetails {
-    pub name: String,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, Hash)]
-#[serde(deny_unknown_fields)]
-pub struct ExternalNetworkSettingBool(bool);
-
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
-#[serde(deny_unknown_fields)]
-pub struct NetworkSettings {
-    #[serde(default, skip_serializing_if = "std::ops::Not::not")]
-    pub attachable: bool,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub driver: Option<String>,
-    #[cfg(feature = "indexmap")]
-    #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
-    pub driver_opts: IndexMap<String, Option<SingleValue>>,
-    #[cfg(not(feature = "indexmap"))]
-    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
-    pub driver_opts: HashMap<String, Option<SingleValue>>,
-    #[serde(default, skip_serializing_if = "std::ops::Not::not")]
-    pub enable_ipv6: bool,
-    #[serde(default, skip_serializing_if = "std::ops::Not::not")]
-    pub internal: bool,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub external: Option<ComposeNetwork>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub ipam: Option<Ipam>,
-    #[serde(default, skip_serializing_if = "Labels::is_empty")]
-    pub labels: Labels,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub name: Option<String>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, Hash)]
-#[serde(deny_unknown_fields)]
-pub struct Ipam {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub driver: Option<String>,
-    #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub config: Vec<IpamConfig>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, Hash)]
-#[serde(deny_unknown_fields)]
-pub struct IpamConfig {
-    pub subnet: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub gateway: Option<String>,
-}
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
 #[serde(deny_unknown_fields)]
