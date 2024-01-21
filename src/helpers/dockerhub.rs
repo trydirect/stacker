d9d4c6609c67488bf21fe7c771e548f89d5df2ea
vsilent <jabberroid@gmail.com>
2024-01-21 11:57:02 +0200

    dockerhub functionality refactoring

--- src/helpers/dockerhub.rs
+++ src/helpers/dockerhub.rs
@@ -1,6 +1,8 @@
+use reqwest::RequestBuilder;
 use serde_derive::{Deserialize, Serialize};
 use serde_valid::Validate;
 use serde_json::Value;
+use crate::forms::stack::DockerImage;
 
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Validate)]
@@ -61,79 +63,35 @@ struct TagResult {
 pub(crate) struct DockerHub<'a> {
     pub(crate) creds: DockerHubCreds<'a>,
     pub(crate) repos: String,
-    pub(crate) token: DockerHubToken,
     pub(crate) image: String,
 }
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Validate)]
-pub(crate) struct DockerHubBuilder<'a> {
-    pub(crate) creds: DockerHubCreds<'a>,
-    pub(crate) repos: &'a str,
-    pub(crate) token: DockerHubToken,
-    pub(crate) image: &'a str,
-}
 
-impl<'a> DockerHubBuilder<'a> {
+impl<'a> DockerHub<'a> {
 
-    pub async fn set_repos(&'a mut self, repo_name: &'a str) -> &'a mut Self {
-        self.repos = repo_name;
-        self
-    }
-
-    pub async fn set_image(&'a mut self, image: &'a str) -> &'a mut Self {
-        self.image = image;
-        self
-    }
-    pub async fn login(&'a mut self, username: &'a str, password: &'a str) -> &'a mut Self {
+    pub async fn login(&'a self) -> Result<DockerHubToken, String> {
         let endpoint = "https://hub.docker.com/v2/users/login";
 
-        self.creds = DockerHubCreds {
-            username: username,
-            password: password
-        };
-
-        if self.creds.password.is_empty() {
-            tracing::debug!("DockerHub credentials were not provided, login not required/image considered public");
-            return self;
-        }
-
-        let response = reqwest::Client::new()
+        reqwest::Client::new()
             .post(endpoint)
             .json(&self.creds)
             .send()
             .await
-            .map_err(|err| format!("{:?}", err))
-            .unwrap()
+            .map_err(|err| format!("{:?}", err))?
             .json::<DockerHubToken>()
             .await
             .map_err(|err| format!("{:?}", err))
-            .map(|token| {
-                self.token = token;
-            });
-
-        tracing::debug!("Login response was: {:?}", response);
-        self
     }
 
     pub async fn is_active(&'a self) -> Result<bool, String> {
+
         // get repo images
         let tags_url = format!("https://hub.docker.com/v2/namespaces/{}/repositories/{}/tags",
                                &self.creds.username, &self.repos);
-
         let mut client = reqwest::Client::new()
             .get(tags_url)
             .header("Accept", "application/json");
 
-        client = match self.token.token.as_ref() {
-            Some(token) => {
-                if !token.is_empty() {
-                    client = client.bearer_auth(token);
-                }
-                client
-            },
-            None => {
-                client
-            }
-        };
+        let mut client = self.set_token(client).await?;
 
         client
             .send()
@@ -155,11 +113,47 @@ impl<'a> DockerHubBuilder<'a> {
             })
     }
 
+    pub async fn set_token(&self, mut client: RequestBuilder) -> Result<RequestBuilder, String> {
+
+        if self.creds.password.is_empty() {
+            return Ok(client);
+        }
+        let token = self.login().await?;
+
+        match token.token {
+            None => {
+                Ok(client)
+            },
+            Some(token) => {
+                Ok(client.bearer_auth(token))
+            }
+        }
+    }
 }
 
-impl<'a> DockerHub<'a>
-{
-    pub fn build() -> DockerHubBuilder<'a> {
-        DockerHubBuilder::default()
+impl<'a> From <&'a DockerImage> for DockerHub<'a> {
+
+    fn from(image: &'a DockerImage) -> Self {
+        let username = match image.dockerhub_user {
+            Some(ref username) => {
+                username
+            },
+            None => ""
+        };
+        let password = match image.dockerhub_password {
+            Some(ref password) => {
+                password
+            },
+            None => ""
+        };
+
+        DockerHub {
+            creds: DockerHubCreds {
+                username: username,
+                password: password
+            },
+            repos: image.dockerhub_name.clone().unwrap_or("".to_string()),
+            image: format!("{}", image),
+        }
     }
 }
