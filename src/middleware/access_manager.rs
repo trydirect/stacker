7348383e97e2cc765a4f338067054fb5a7127b10
petru <smart__petea@mail.ru>
2024-01-29 17:51:54 +0200

    issue-auth casbin adapter

--- src/middleware/access_manager.rs
+++ src/middleware/access_manager.rs
@@ -1,14 +1,21 @@
-use actix_casbin_auth::casbin::{DefaultModel, FileAdapter};//, Result};
-use actix_casbin_auth::CasbinService;
-use actix_casbin_auth::casbin::function_map::key_match2;
-use actix_casbin_auth::casbin::CoreApi;
+use actix_casbin_auth::{
+    CasbinService,
+    casbin::{
+        DefaultModel,
+        CoreApi,
+        function_map::key_match2
+    }
+};
 use std::io::{Error, ErrorKind};
+use sqlx_adapter::SqlxAdapter;
 
-pub async fn try_new() -> Result<CasbinService, Error> {
+pub async fn try_new(db_connection_address: String) -> Result<CasbinService, Error> {
     let m = DefaultModel::from_file("rbac/rbac_with_pattern_model.conf")
         .await
         .map_err(|err| Error::new(ErrorKind::Other, format!("{err:?}")))?;
-    let a = FileAdapter::new("rbac/rbac_with_pattern_policy.csv");  
+    let a = SqlxAdapter::new(db_connection_address, 8)
+        .await
+        .map_err(|err| Error::new(ErrorKind::Other, format!("{err:?}")))?;
 
     let mut casbin_service = CasbinService::new(m, a)
         .await
