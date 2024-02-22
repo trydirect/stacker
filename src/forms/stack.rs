33224e2c776d1dbc04d25ea0e5e70dfb13932d37
vsilent <jabberroid@gmail.com>
2024-02-22 11:42:34 +0200

    allow float numbers for disk_size and ram_size

--- src/forms/stack.rs
+++ src/forms/stack.rs
@@ -16,17 +16,17 @@ pub struct Role {
 pub struct Requirements {
     #[validate(min_length = 1)]
     #[validate(max_length = 10)]
-    #[validate(pattern = r"^\d+\.?[0-9]+$")]
+    #[validate(pattern = r"^[0-9]*\.?[0-9]+$")]
     pub cpu: Option<String>,
     #[validate(min_length = 1)]
     #[validate(max_length = 10)]
-    #[validate(pattern = r"^\d+Gb?$")]
+    #[validate(pattern = r"^[0-9]*\.?[0-9]+Gb?$")]
     #[serde(rename = "disk_size")]
     pub disk_size: Option<String>,
     #[serde(rename = "ram_size")]
     #[validate(min_length = 1)]
     #[validate(max_length = 10)]
-    #[validate(pattern = r"^\d+Gb?$")]
+    #[validate(pattern = r"^[0-9]*\.?[0-9]+Gb?$")]
     pub ram_size: Option<String>,
 }
 
