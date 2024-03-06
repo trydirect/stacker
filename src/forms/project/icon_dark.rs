229084ec0d8fface90f31bab937e5466ed6d9898
vsilent <jabberroid@gmail.com>
2024-03-06 13:24:05 +0200

    icon attrs

--- src/forms/project/icon_dark.rs
+++ src/forms/project/icon_dark.rs
@@ -1,4 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct IconDark {}
+pub struct IconDark {
+    width: Option<i32>,
+    height: Option<i32>,
+    image: Option<String>
+}
--- src/forms/project/icon_light.rs
+++ src/forms/project/icon_light.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct IconLight {
-    pub width: i64,
-    pub height: i64,
-    pub image: String,
+    pub width: Option<i64>,
+    pub height: Option<i64>,
+    pub image: Option<String>,
 }
