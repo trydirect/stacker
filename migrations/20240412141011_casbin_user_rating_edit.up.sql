6479455e5e6dc88aab2c815c392adc0d8dbd1665
petru <smart__petea@mail.ru>
2024-04-12 21:46:38 +0300

    47 admin_list_handler, transaction

--- migrations/20240412141011_casbin_user_rating_edit.up.sql
+++ migrations/20240412141011_casbin_user_rating_edit.up.sql
@@ -1,4 +1,6 @@
 -- Add up migration script here
+BEGIN TRANSACTION;
+
 INSERT INTO casbin_rule
 (id, ptype, v0, v1, v2, v3, v4, v5)
 VALUES((select max(id) + 1 from casbin_rule cr), 'p', 'group_user', '/rating/:id', 'PUT', '', '', '');
@@ -6,3 +8,9 @@ VALUES((select max(id) + 1 from casbin_rule cr), 'p', 'group_user', '/rating/:id
 INSERT INTO casbin_rule
 (id, ptype, v0, v1, v2, v3, v4, v5)
 VALUES((select max(id) + 1 from casbin_rule cr), 'p', 'group_admin', '/admin/rating/:id', 'GET', '', '', '');
+
+INSERT INTO casbin_rule
+(id, ptype, v0, v1, v2, v3, v4, v5)
+VALUES((select max(id) + 1 from casbin_rule cr), 'p', 'group_admin', '/admin/rating', 'GET', '', '', '');
+
+COMMIT TRANSACTION;
