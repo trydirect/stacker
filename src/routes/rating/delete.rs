07e0490b34e0fbae94ac8d2374fcb1da687cde62
petru <smart__petea@mail.ru>
2024-04-12 22:03:51 +0300

    47 user delete rating. partially

---
+++ src/routes/rating/delete.rs
@@ -0,0 +1,25 @@
+use crate::db;
+use crate::helpers::JsonResponse;
+use crate::models;
+use actix_web::{delete, web, Responder, Result};
+use sqlx::PgPool;
+
+#[tracing::instrument(name = "User delete rating.")]
+#[delete("/{id}")]
+pub async fn user_delete_handler(
+    path: web::Path<(i32,)>,
+    pg_pool: web::Data<PgPool>,
+) -> Result<impl Responder> {
+    let rate_id = path.0;
+    let rating = db::rating::fetch(pg_pool.get_ref(), rate_id)
+        .await
+        .map_err(|_err| JsonResponse::<models::Rating>::build().internal_server_error(""))
+        .and_then(|rating| {
+            match rating {
+                Some(rating) if rating.hidden == Some(false) => { Ok(rating) },
+                _ => Err(JsonResponse::<models::Rating>::build().not_found("not found"))
+            }
+        })?;
+
+    Ok(JsonResponse::build().set_item(rating).ok("OK"))
+}
