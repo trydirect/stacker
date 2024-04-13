02ec72fcb1bc42c0d23adb4df29ef08188fa0125
petru <smart__petea@mail.ru>
2024-04-13 15:46:53 +0300

    47 rating admin edit handler

--- migrations/20240412141011_casbin_user_rating_edit.down.sql
+++ migrations/20240412141011_casbin_user_rating_edit.down.sql
@@ -3,6 +3,9 @@ DELETE FROM casbin_rule
 WHERE ptype = 'p' and v0 = 'group_user' and v1 = '/rating/:id' and v2 = 'PUT';
 
 DELETE FROM casbin_rule
+WHERE ptype = 'p' and v0 = 'group_admin' and v1 = '/admin/rating/:id' and v2 = 'PUT';
+
+DELETE FROM casbin_rule
 WHERE ptype = 'p' and v0 = 'group_user' and v1 = '/rating/:id' and v2 = 'DELETE';
 
 DELETE FROM casbin_rule
--- migrations/20240412141011_casbin_user_rating_edit.up.sql
+++ migrations/20240412141011_casbin_user_rating_edit.up.sql
@@ -7,6 +7,10 @@ VALUES((select max(id) + 1 from casbin_rule cr), 'p', 'group_user', '/rating/:id
 
 INSERT INTO casbin_rule
 (id, ptype, v0, v1, v2, v3, v4, v5)
+VALUES((select max(id) + 1 from casbin_rule cr), 'p', 'group_admin', '/admin/rating/:id', 'PUT', '', '', '');
+
+INSERT INTO casbin_rule
+(id, ptype, v0, v1, v2, v3, v4, v5)
 VALUES((select max(id) + 1 from casbin_rule cr), 'p', 'group_user', '/rating/:id', 'DELETE', '', '', '');
 
 INSERT INTO casbin_rule
