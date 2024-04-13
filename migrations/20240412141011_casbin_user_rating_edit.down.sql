5255b65102f756009f23c194e098df22d1792b01
petru <smart__petea@mail.ru>
2024-04-13 15:55:44 +0300

    47 delete rating by admin

--- migrations/20240412141011_casbin_user_rating_edit.down.sql
+++ migrations/20240412141011_casbin_user_rating_edit.down.sql
@@ -9,6 +9,9 @@ DELETE FROM casbin_rule
 WHERE ptype = 'p' and v0 = 'group_user' and v1 = '/rating/:id' and v2 = 'DELETE';
 
 DELETE FROM casbin_rule
+WHERE ptype = 'p' and v0 = 'group_admin' and v1 = '/admin/rating/:id' and v2 = 'DELETE';
+
+DELETE FROM casbin_rule
 WHERE ptype = 'p' and v0 = 'group_admin' and v1 = '/admin/rating/:id' and v2 = 'GET';
 
 DELETE FROM casbin_rule
