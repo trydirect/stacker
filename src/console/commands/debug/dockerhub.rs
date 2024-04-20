16f027bb53938e526dd5a8eab8aff873b8aecfad
petru <smart__petea@mail.ru>
2024-04-20 07:23:16 +0300

    54 is_active with tokio::select!

--- src/console/commands/debug/dockerhub.rs
+++ src/console/commands/debug/dockerhub.rs
@@ -26,43 +26,9 @@ impl crate::console::commands::CallableTrait for DockerhubCommand {
             println!("{}", self.json);
             let dockerImage: DockerImage = serde_json::from_str(&self.json)?;
             let mut dockerhub = DockerHub::try_from(&dockerImage)?;
-            /*
-            if dockerhub.creds.username.is_empty() {
-            */
-            let lookupOfficialRepos = dockerhub.lookup_official_repos();
-            let lookupPublicRepos = dockerhub.lookup_public_repos();
-            let lookupPublicRepos1 = dockerhub.lookup_public_repos();
-            tokio::select! {
-                Ok(true) = lookupOfficialRepos => {println!("lookup oficial repos"); }
-                Ok(true) = lookupPublicRepos => {println!("lookup public repos"); }
-                Ok(true) = lookupPublicRepos1 => {println!("lookup public repos1"); }
-                //todo add timer
-                //complete => { println!("complete"); }
-                //default => { println!("no good result"); }
-                else => { println!("nothing found") }
-            }
-            /*
-                if Ok(true) == dockerhub.lookup_official_repos().await {
-                    println!("official: true");
-                    return Ok(());
-                } else {
-                    println!("official: false");
-                };
+            let isActive = dockerhub.is_active().await?;
 
-                if Ok(true) == dockerhub.lookup_public_repos().await {
-                    println!("public: true");
-                    return Ok(());
-                };
-                */
-                /*
-            } else {
-                println!("username is not empty");
-                if Ok(true) == dockerhub.lookup_private_repo().await {
-                    tracing::debug!("private: true");
-                    return Ok(());
-                };
-            }
-            */
+            println!("image is active: {isActive}");
 
             Ok(())
         })
