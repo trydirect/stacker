a94b1cb12d78cbfaa856812b7ae8edef8204fa68
vsilent <jabberroid@gmail.com>
2024-07-18 12:44:58 +0300

    agreement accept, and management for admin api

---
+++ src/routes/agreement/update.rs
@@ -0,0 +1,48 @@
+use crate::forms;
+use crate::views;
+use crate::helpers::JsonResponse;
+use crate::models;
+use crate::db;
+use actix_web::{post, put, web, Responder, Result};
+use sqlx::PgPool;
+use std::sync::Arc;
+use serde_valid::Validate;
+
+
+#[tracing::instrument(name = "Admin edit agreement.")]
+#[put("/{id}")]
+pub async fn admin_edit_handler(
+    path: web::Path<(i32,)>,
+    form: web::Json<forms::agreement::AdminAddAgreement>,
+    pg_pool: web::Data<PgPool>,
+) -> Result<impl Responder> {
+    if let Err(errors) = form.validate() {
+        return Err(JsonResponse::<models::Agreement>::build().form_error(errors.to_string()));
+    }
+
+    let id = path.0;
+    let mut item = db::agreement::fetch(pg_pool.get_ref(), id)
+        .await
+        .map_err(|_err| JsonResponse::<models::Agreement>::build().internal_server_error(""))
+        .and_then(|item| {
+            match item {
+                Some(item) =>  Ok(item),
+                _ => Err(JsonResponse::<models::Agreement>::build().not_found("not found"))
+            }
+        })?;
+
+    form.into_inner().update(&mut item);
+
+    db::agreement::update(pg_pool.get_ref(), item)
+        .await
+        .map(|item| {
+            JsonResponse::<models::Agreement>::build()
+                .set_item(Into::<models::Agreement>::into(item))
+                .ok("success")
+        })
+        .map_err(|err| {
+            tracing::error!("Failed to execute query: {:?}", err);
+            JsonResponse::<models::Agreement>::build().internal_server_error("Agreement not updated")
+        })
+}
+
