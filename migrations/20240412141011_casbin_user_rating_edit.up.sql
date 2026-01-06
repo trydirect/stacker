203f7299d320f8ace15b1809e68ac67e9026b6d4
vsilent <jabberroid@gmail.com>
2026-01-06 16:36:56 +0200

    migrations revert

--- migrations/20240412141011_casbin_user_rating_edit.up.sql
+++ migrations/20240412141011_casbin_user_rating_edit.up.sql
@@ -1,24 +1,18 @@
 -- Add up migration script here
 INSERT INTO casbin_rule (ptype, v0, v1, v2, v3, v4, v5)
-VALUES ('p', 'group_user', '/rating/:id', 'PUT', '', '', '')
-ON CONFLICT DO NOTHING;
+VALUES ('p', 'group_user', '/rating/:id', 'PUT', '', '', '');
 
 INSERT INTO casbin_rule (ptype, v0, v1, v2, v3, v4, v5)
-VALUES ('p', 'group_admin', '/admin/rating/:id', 'PUT', '', '', '')
-ON CONFLICT DO NOTHING;
+VALUES ('p', 'group_admin', '/admin/rating/:id', 'PUT', '', '', '');
 
 INSERT INTO casbin_rule (ptype, v0, v1, v2, v3, v4, v5)
-VALUES ('p', 'group_user', '/rating/:id', 'DELETE', '', '', '')
-ON CONFLICT DO NOTHING;
+VALUES ('p', 'group_user', '/rating/:id', 'DELETE', '', '', '');
 
 INSERT INTO casbin_rule (ptype, v0, v1, v2, v3, v4, v5)
-VALUES ('p', 'group_admin', '/admin/rating/:id', 'DELETE', '', '', '')
-ON CONFLICT DO NOTHING;
+VALUES ('p', 'group_admin', '/admin/rating/:id', 'DELETE', '', '', '');
 
 INSERT INTO casbin_rule (ptype, v0, v1, v2, v3, v4, v5)
-VALUES ('p', 'group_admin', '/admin/rating/:id', 'GET', '', '', '')
-ON CONFLICT DO NOTHING;
+VALUES ('p', 'group_admin', '/admin/rating/:id', 'GET', '', '', '');
 
 INSERT INTO casbin_rule (ptype, v0, v1, v2, v3, v4, v5)
-VALUES ('p', 'group_admin', '/admin/rating', 'GET', '', '', '')
-ON CONFLICT DO NOTHING;
+VALUES ('p', 'group_admin', '/admin/rating', 'GET', '', '', '');
