2ef58e4af88b5ed0faf84ea3591e33fd56478207
Vasili Pascal <vasili.pascal@gmail.com>
2026-06-22 22:57:14 +0300

    fix: case-insensitive JOIN on creator_user_id, match vendor by public_slug OR creator_user_id

--- migrations/20260718128000_casbin_admin_vendor_profile_by_creator_rule.down.sql
+++ migrations/20260718128000_casbin_admin_vendor_profile_by_creator_rule.down.sql
@@ -1,7 +1,4 @@
 DELETE FROM public.casbin_rule
 WHERE ptype = 'p'
-  AND v1 IN (
-      '/api/admin/vendors/:creator_user_id/vendor-profile',
-      '/stacker/api/admin/vendors/:creator_user_id/vendor-profile'
-  )
+  AND v1 = '/api/admin/vendors/:creator_user_id/vendor-profile'
   AND v2 = 'PATCH';
