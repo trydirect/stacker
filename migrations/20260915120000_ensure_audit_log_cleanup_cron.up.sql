b2a9580a8f610627268f97ce559f6c817184a7d6
Vasili Pascal <vasili.pascal@gmail.com>
2026-09-15 14:02:49 +0300

    fix(db): guard optional cron job lookup

--- migrations/20260915120000_ensure_audit_log_cleanup_cron.up.sql
+++ migrations/20260915120000_ensure_audit_log_cleanup_cron.up.sql
@@ -10,19 +10,21 @@ $$;
 
 DO $$
 BEGIN
-    IF EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'pg_cron')
-       AND EXISTS (
-           SELECT 1 FROM pg_proc
-           WHERE proname = 'stacker_cleanup_audit_logs'
-       )
-       AND NOT EXISTS (
-           SELECT 1 FROM cron.job WHERE jobname = 'stacker_cleanup_audit_logs'
-       ) THEN
-        PERFORM cron.schedule(
-            'stacker_cleanup_audit_logs',
-            '0 4 * * *',
-            $cron$SELECT stacker_cleanup_audit_logs(p_dry_run := false);$cron$
-        );
+    IF EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'pg_cron') THEN
+        IF EXISTS (
+            SELECT 1 FROM pg_proc
+            WHERE proname = 'stacker_cleanup_audit_logs'
+        ) THEN
+            IF NOT EXISTS (
+                SELECT 1 FROM cron.job WHERE jobname = 'stacker_cleanup_audit_logs'
+            ) THEN
+                PERFORM cron.schedule(
+                    'stacker_cleanup_audit_logs',
+                    '0 4 * * *',
+                    $cron$SELECT stacker_cleanup_audit_logs(p_dry_run := false);$cron$
+                );
+            END IF;
+        END IF;
     END IF;
 END;
 $$;
